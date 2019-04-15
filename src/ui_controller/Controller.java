14f62b02856da2ddb2ed1903dee24d767dc57153
phoenixrifl <phoenixrifl972@gmail.com>
2019-04-15 18:01:51 +0200

    debut

---
+++ src/ui_controller/Controller.java
@@ -0,0 +1,5 @@
+package ui_controller;
+
+public class Controller {
+
+}
---
+++ src/ui_controller/Ui.java
@@ -0,0 +1,14 @@
+package ui_controller;
+
+public class Ui {
+	
+	private int choix;
+
+	public Ui(int choix) {
+		this.choix = choix;
+	}
+	
+	public int getChoix() {
+		return choix;
+	}
+}
