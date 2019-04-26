882474e84eff975dfe549bdd3e6af7b721d8a8c5
phoenixrifl <phoenixrifl972@gmail.com>
2019-04-26 17:31:51 +0200

    first Test DAO

--- src/test/java/CompanyDAOTest.java
+++ src/test/java/CompanyDAOTest.java
@@ -1,13 +1,21 @@
 package test.java;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import main.java.persistence.CompanyDAO;
 
 public class CompanyDAOTest {
-//	
-//	private CompanyDAO companyDAO;
-//	
-//	@Before
-//	public void setUpBefore() {
-//		companyDAO = 
-//	}
+	
+	private CompanyDAO companyDAO;
+	
+	@Before
+	public void setUpBefore() {
+		companyDAO = CompanyDAO.getInstance();
+	}
+	
+	@Test
+	public void TestFindAll() {
+		companyDAO.findAll();
+	}
 }
