2d356e119a3d2e84ef319c05e7c5154d9d8c3fd0
phoenixrifl <phoenixrifl972@gmail.com>
2019-06-04 18:07:02 +0200

    En cours multimod

---
+++ webapp/src/main/java/servlet/AddComputer.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import CompanyService;
+import ComputerService;
+import dto.ComputerDTO;
+import exception.SqlCommandeException;
+import validator.ComputerValidator;
+
+@Controller
+public class AddComputer extends Servlet {
+	private static final long serialVersionUID = 1L;
+	private CompanyService companyService;
+	private ComputerService computerService;
+	private ComputerValidator computerValidator;
+
+	public AddComputer(ComputerService computerService, CompanyService companyService,
+			ComputerValidator computerValidator) {
+		this.computerService = computerService;
+		this.companyService = companyService;
+		this.computerValidator = computerValidator;
+	}
+
+	@GetMapping(value = { "/addComputer" })
+	public String doGet(Model model) throws SqlCommandeException {
+		model.addAttribute("listCompany", companyService.findAll());
+		return "addComputer";
+	}
+
+	@PostMapping(value = { "/addComputer" })
+	public RedirectView doPost(@RequestParam(value = "computerName", required = false) String computerName,
+			@RequestParam(value = "introduced", required = false) String introduced,
+			@RequestParam(value = "discontinued", required = false) String discontinued,
+			@RequestParam(value = "companyId", required = false) Long companyId, Model model) {
+
+		ComputerDTO computerDTO = new ComputerDTO(computerName, introduced, discontinued, companyId);
+		if (computerValidator.isAComputerValid(computerDTO)) {
+			if (computerService.create(computerDTO))
+				model.addAttribute("reussite", "Insertion reussite");
+			else
+				model.addAttribute("echec", "Echec insertion");
+		}
+
+		return new RedirectView("dashboard");
+	}
+}
---
+++ webapp/src/main/java/servlet/ServletDashboard.java
@@ -0,0 +1,100 @@
+package servlet;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import ComputerService;
+import dto.ComputerDTO;
+import exception.SqlCommandeException;
+import servlet.model.Pagination;
+
+@Controller
+@SessionAttributes(value = ServletDashboard.PAGINATION, types = { Pagination.class })
+public class ServletDashboard extends Servlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected static final String PAGINATION = "pagination";
+	private ComputerService computerService;
+
+	public ServletDashboard(ComputerService computerService) {
+		this.computerService = computerService;
+	}
+
+	@ModelAttribute(PAGINATION)
+	public Pagination paginationInstance() {
+		return new Pagination();
+	}
+
+	@GetMapping(value = { "/", "/dashboard" })
+	public String getParam(@RequestParam(value = "page", required = false) Integer page,
+			@RequestParam(value = "limit", required = false) Integer limit,
+			@RequestParam(value = "search", required = false) String search,
+			@RequestParam(value = "orderbycolumn", required = false) String orderbycolumn,
+			@ModelAttribute(PAGINATION) Pagination pagination, Model model) throws SqlCommandeException {
+
+		if (page == null) {
+			pagination.setPage(1);
+		} else {
+			pagination.setPage(page);
+
+		}
+		if (search != null) {
+			pagination.setSearch(search);
+
+		}
+
+		if (limit == null) {
+			pagination.setLimit(10);
+
+		} else {
+			pagination.setLimit(limit);
+			pagination.setOffset(pagination.getLimit() * (pagination.getPage() - 1));
+
+		}
+
+		if (orderbycolumn != null) {
+
+			pagination.setOrderby(orderbycolumn);
+
+		}
+
+		List<ComputerDTO> computerDTO_list = null;
+		long nbTotalComputers = 0;
+		if (search == null || search.isEmpty() || search.equals("dashboard")) {
+			computerDTO_list = computerService.findAll(pagination);
+			nbTotalComputers = computerService.count();
+		} else {
+			computerDTO_list = computerService.find(pagination);
+			nbTotalComputers = computerService.countSearch(pagination);
+
+		}
+		pagination.setNbTotalComputers(nbTotalComputers);
+		pagination.pageview();
+
+		model.addAttribute("computers", computerDTO_list);
+
+		return "dashboard";
+
+	}
+
+	@PostMapping(value = { "/", "/dashboard" })
+	public RedirectView doPost(@RequestParam(value = "selection", required = false) String selection, Model model) {
+		String[] spliteur = selection.split(",");
+		for (int i = 0; i < spliteur.length; i++) {
+			computerService.delete(Long.parseLong(spliteur[i]));
+		}
+		return new RedirectView("dashboard");
+
+	}
+}
